64df8b0f390f12506580d5f38e14d040e64e41a6
planit-backend <planit-backend@tanjeffreyz.github.io>
2022-09-04 22:34:03 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -64,8 +64,8 @@ assignments = {
       "title": "Quick Check 2",
       "course": "Data 100",
       "dueDate": "2022-09-05T23:59:00",
-      "link": "https://www.gradescope.com/courses/422877/assignments/2217765/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/422877/assignments/2217765/submissions/131948366",
+      "submitted": true
     }
   ],
   "Data 140": [
