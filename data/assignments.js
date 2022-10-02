25b9a749276594d8137e345ff70bf4b072e8e7b4
planit <planit@tanjeffreyz.github.io>
2022-10-02 17:48:07 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -183,6 +183,13 @@ assignments = {
       "dueDate": "2022-10-06T23:59:00",
       "link": "https://www.gradescope.com/courses/422877",
       "submitted": false
+    },
+    {
+      "title": "Lab 7",
+      "course": "Data 100",
+      "dueDate": "2022-07-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877",
+      "submitted": false
     }
   ],
   "Data 140": [
