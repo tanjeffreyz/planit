a189c44147809e1bb977ea37eae2e5f25b6be6f4
planit <planit@tanjeffreyz.github.io>
2022-10-15 03:17:00 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -220,20 +220,6 @@ assignments = {
       "submitted": false
     },
     {
-      "title": "Project 1B",
-      "course": "Data 100",
-      "dueDate": "2022-10-20T23:59:00",
-      "link": "https://www.gradescope.com/courses/422877",
-      "submitted": false
-    },
-    {
-      "title": "Project 1B Test Set Predictions",
-      "course": "Data 100",
-      "dueDate": "2022-10-20T23:59:00",
-      "link": "https://www.gradescope.com/courses/422877",
-      "submitted": false
-    },
-    {
       "title": "Quick Check 8",
       "course": "Data 100",
       "dueDate": "2022-10-17T23:59:00",
