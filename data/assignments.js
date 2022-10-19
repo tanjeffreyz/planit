8dc01d5549d21457bfb9eb2384aae12c9e64008f
planit <planit@tanjeffreyz.github.io>
2022-10-19 03:18:33 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -216,8 +216,8 @@ assignments = {
       "title": "Lab 8",
       "course": "Data 100",
       "dueDate": "2022-10-18T23:59:00",
-      "link": "https://www.gradescope.com/courses/422877",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/422877/assignments/2334327/submissions/141928355",
+      "submitted": true
     },
     {
       "title": "Quick Check 8",
