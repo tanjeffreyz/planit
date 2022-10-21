ecb243ab11f7a7132e87fca31a5b391940fb7a8b
planit <planit@tanjeffreyz.github.io>
2022-10-21 02:38:18 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -241,6 +241,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "Quick Check 9",
+      "course": "Data 100",
+      "dueDate": "2022-10-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2364500/submissions/new",
+      "submitted": false
+    },
+    {
       "title": "Midterm Online Exam",
       "course": "Data 100",
       "dueDate": "2022-10-20T23:59:00",
