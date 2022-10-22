b3c6ffaeaac8dd10b9eb7064a85fd8edc6919690
planit <planit@tanjeffreyz.github.io>
2022-10-22 14:44:44 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -174,7 +174,7 @@ assignments = {
       "title": "Grad Project Checkpoint 1",
       "course": "Data 100",
       "dueDate": "2022-10-21T23:59:00",
-      "link": "https://www.gradescope.com/courses/422877",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2306444/submissions/new",
       "submitted": false
     },
     {
