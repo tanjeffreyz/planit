2a0921c5844b8810bc46f7a13ae5d111d9e5276e
planit <planit@tanjeffreyz.github.io>
2022-11-02 00:30:35 +0000

    Updated assignments

---
+++ data/assignments.js
@@ -0,0 +1,442 @@
+assignments = {
+  "CS 161": [
+    {
+      "title": "Homework 1",
+      "course": "CS 161",
+      "dueDate": "2022-09-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/419634/assignments/2209664/submissions/131225850",
+      "submitted": true
+    },
+    {
+      "title": "Project 1 Writeup",
+      "course": "CS 161",
+      "dueDate": "2022-09-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/419634/assignments/2250239/submissions/138274835",
+      "submitted": true
+    },
+    {
+      "title": "Homework 2",
+      "course": "CS 161",
+      "dueDate": "2022-09-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/419634/assignments/2250878/submissions/134142498",
+      "submitted": true
+    },
+    {
+      "title": "Project 1 Checkpoint",
+      "course": "CS 161",
+      "dueDate": "2022-09-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/419634/assignments/2257656/submissions/134664996",
+      "submitted": true
+    },
+    {
+      "title": "Project 1 Autograder",
+      "course": "CS 161",
+      "dueDate": "2022-09-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/419634/assignments/2279510/submissions/137998806",
+      "submitted": true
+    },
+    {
+      "title": "Homework 3",
+      "course": "CS 161",
+      "dueDate": "2022-10-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/419634/assignments/2302454/submissions/139334613",
+      "submitted": true
+    },
+    {
+      "title": "Homework 3 Lab Autograder",
+      "course": "CS 161",
+      "dueDate": "2022-10-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/419634/assignments/2303618/submissions/139892003",
+      "submitted": true
+    },
+    {
+      "title": "Homework 4",
+      "course": "CS 161",
+      "dueDate": "2022-10-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/419634/assignments/2346817/submissions/143313301",
+      "submitted": true
+    },
+    {
+      "title": "Homework 5",
+      "course": "CS 161",
+      "dueDate": "2022-11-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/419634/assignments/2384298/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Project 2 Design Doc Checkpoint",
+      "course": "CS 161",
+      "dueDate": "2022-10-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/419634",
+      "submitted": false
+    }
+  ],
+  "Data 100": [
+    {
+      "title": "Lab 01",
+      "course": "Data 100",
+      "dueDate": "2022-08-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2190132/submissions/131011288",
+      "submitted": true
+    },
+    {
+      "title": "Quick Check 1",
+      "course": "Data 100",
+      "dueDate": "2022-08-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2193231/submissions/130861192",
+      "submitted": true
+    },
+    {
+      "title": "Homework 01",
+      "course": "Data 100",
+      "dueDate": "2022-09-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2196379/submissions/131641007",
+      "submitted": true
+    },
+    {
+      "title": "Homework 2",
+      "course": "Data 100",
+      "dueDate": "2022-09-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2207078/submissions/132614879",
+      "submitted": true
+    },
+    {
+      "title": "Lab 2",
+      "course": "Data 100",
+      "dueDate": "2022-09-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2215881/submissions/132408027",
+      "submitted": true
+    },
+    {
+      "title": "Quick Check 2",
+      "course": "Data 100",
+      "dueDate": "2022-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2217765/submissions/131948366",
+      "submitted": true
+    },
+    {
+      "title": "Homework 3",
+      "course": "Data 100",
+      "dueDate": "2022-09-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2222764/submissions/134135319",
+      "submitted": true
+    },
+    {
+      "title": "Lab 03",
+      "course": "Data 100",
+      "dueDate": "2022-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2234463/submissions/133523523",
+      "submitted": true
+    },
+    {
+      "title": "Quick Check 3",
+      "course": "Data 100",
+      "dueDate": "2022-09-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2235952/submissions/133031441",
+      "submitted": true
+    },
+    {
+      "title": "Quick Check 4",
+      "course": "Data 100",
+      "dueDate": "2022-09-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2241956/submissions/134752358",
+      "submitted": true
+    },
+    {
+      "title": "Lab 4",
+      "course": "Data 100",
+      "dueDate": "2022-09-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2251063/submissions/135190817",
+      "submitted": true
+    },
+    {
+      "title": "Homework 4",
+      "course": "Data 100",
+      "dueDate": "2022-09-22T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2251099/submissions/135703003",
+      "submitted": true
+    },
+    {
+      "title": "Quick Check 5",
+      "course": "Data 100",
+      "dueDate": "2022-09-26T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2279583/submissions/136447889",
+      "submitted": true
+    },
+    {
+      "title": "Lab 5",
+      "course": "Data 100",
+      "dueDate": "2022-09-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2284700/submissions/136745512",
+      "submitted": true
+    },
+    {
+      "title": "Homework 5",
+      "course": "Data 100",
+      "dueDate": "2022-09-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2289653/submissions/137406739",
+      "submitted": true
+    },
+    {
+      "title": "Quick Check 6 ",
+      "course": "Data 100",
+      "dueDate": "2022-10-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2300738/submissions/138128902",
+      "submitted": true
+    },
+    {
+      "title": "Grad Project Checkpoint 1",
+      "course": "Data 100",
+      "dueDate": "2022-10-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877",
+      "submitted": false
+    },
+    {
+      "title": "Lab 6",
+      "course": "Data 100",
+      "dueDate": "2022-10-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2307591/submissions/138481763",
+      "submitted": true
+    },
+    {
+      "title": "Homework 6",
+      "course": "Data 100",
+      "dueDate": "2022-10-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2312062/submissions/139102250",
+      "submitted": true
+    },
+    {
+      "title": "Lab 7",
+      "course": "Data 100",
+      "dueDate": "2022-10-11T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2314211/submissions/140158032",
+      "submitted": true
+    },
+    {
+      "title": "Project 1A",
+      "course": "Data 100",
+      "dueDate": "2022-10-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2314278/submissions/140773740",
+      "submitted": true
+    },
+    {
+      "title": "Overall Grade",
+      "course": "Data 100",
+      "dueDate": "2022-12-22T13:00:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2317321/submissions/144679025",
+      "submitted": true
+    },
+    {
+      "title": "Discussion",
+      "course": "Data 100",
+      "dueDate": "2022-12-22T13:00:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2317951/submissions/144671599",
+      "submitted": true
+    },
+    {
+      "title": "Quick Check 7",
+      "course": "Data 100",
+      "dueDate": "2022-10-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2321933/submissions/139789059",
+      "submitted": true
+    },
+    {
+      "title": "Lab 8",
+      "course": "Data 100",
+      "dueDate": "2022-10-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2334327/submissions/141928355",
+      "submitted": true
+    },
+    {
+      "title": "Project 1B",
+      "course": "Data 100",
+      "dueDate": "2022-10-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2334512/submissions/144262646",
+      "submitted": true
+    },
+    {
+      "title": "Project 1B Test Set Predictions",
+      "course": "Data 100",
+      "dueDate": "2022-10-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2334513/submissions/144254069",
+      "submitted": true
+    },
+    {
+      "title": "Quick Check 8",
+      "course": "Data 100",
+      "dueDate": "2022-10-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2345436/submissions/141453641",
+      "submitted": true
+    },
+    {
+      "title": "Lab 9",
+      "course": "Data 100",
+      "dueDate": "2022-10-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2363497/submissions/143828349",
+      "submitted": true
+    },
+    {
+      "title": "Quick Check 9",
+      "course": "Data 100",
+      "dueDate": "2022-10-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2364500/submissions/143136004",
+      "submitted": true
+    },
+    {
+      "title": "Midterm Online Exam",
+      "course": "Data 100",
+      "dueDate": "2022-10-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877",
+      "submitted": false
+    },
+    {
+      "title": "Lab 10",
+      "course": "Data 100",
+      "dueDate": "2022-11-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877",
+      "submitted": false
+    },
+    {
+      "title": "Homework 7",
+      "course": "Data 100",
+      "dueDate": "2022-11-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877",
+      "submitted": false
+    },
+    {
+      "title": "Homework 7 Written (Student Submission)",
+      "course": "Data 100",
+      "dueDate": "2022-11-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877",
+      "submitted": false
+    },
+    {
+      "title": "Quick Check 10",
+      "course": "Data 100",
+      "dueDate": "2022-10-31T23:59:00",
+      "link": "https://www.gradescope.com/courses/422877/assignments/2386762/submissions/145360074",
+      "submitted": true
+    }
+  ],
+  "Data 140": [
+    {
+      "title": "Homework 01",
+      "course": "Data 140",
+      "dueDate": "2022-08-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2199989/submissions/131181712",
+      "submitted": true
+    },
+    {
+      "title": "Lab 01",
+      "course": "Data 140",
+      "dueDate": "2022-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2199991/submissions/131815478",
+      "submitted": true
+    },
+    {
+      "title": "Homework 02",
+      "course": "Data 140",
+      "dueDate": "2022-09-06T12:00:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2221343/submissions/132150932",
+      "submitted": true
+    },
+    {
+      "title": "Homework 03",
+      "course": "Data 140",
+      "dueDate": "2022-09-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2238053/submissions/133371538",
+      "submitted": true
+    },
+    {
+      "title": "Lab 02A",
+      "course": "Data 140",
+      "dueDate": "2022-09-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2238129/submissions/133223987",
+      "submitted": true
+    },
+    {
+      "title": "Homework 04",
+      "course": "Data 140",
+      "dueDate": "2022-09-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2257826/submissions/134938809",
+      "submitted": true
+    },
+    {
+      "title": "Lab 02B",
+      "course": "Data 140",
+      "dueDate": "2022-09-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2257858/submissions/134876651",
+      "submitted": true
+    },
+    {
+      "title": "Homework 05",
+      "course": "Data 140",
+      "dueDate": "2022-09-26T23:59:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2275916/submissions/136341533",
+      "submitted": true
+    },
+    {
+      "title": "Homework 06",
+      "course": "Data 140",
+      "dueDate": "2022-10-03T12:00:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2298985/submissions/137914399",
+      "submitted": true
+    },
+    {
+      "title": "Lab 03",
+      "course": "Data 140",
+      "dueDate": "2022-10-03T12:00:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2299005/submissions/137649315",
+      "submitted": true
+    },
+    {
+      "title": "Homework 07",
+      "course": "Data 140",
+      "dueDate": "2022-10-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2329684/submissions/139825645",
+      "submitted": true
+    },
+    {
+      "title": "Homework 08",
+      "course": "Data 140",
+      "dueDate": "2022-10-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2346316/submissions/141464567",
+      "submitted": true
+    },
+    {
+      "title": "Lab 04",
+      "course": "Data 140",
+      "dueDate": "2022-10-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2346576/submissions/141650203",
+      "submitted": true
+    },
+    {
+      "title": "Homework 09",
+      "course": "Data 140",
+      "dueDate": "2022-10-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2363013/submissions/143341183",
+      "submitted": true
+    },
+    {
+      "title": "Lab 05A",
+      "course": "Data 140",
+      "dueDate": "2022-10-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2363039/submissions/143611469",
+      "submitted": true
+    },
+    {
+      "title": "Lab 05B",
+      "course": "Data 140",
+      "dueDate": "2022-10-31T23:59:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2386628/submissions/145458336",
+      "submitted": true
+    },
+    {
+      "title": "Homework 10",
+      "course": "Data 140",
+      "dueDate": "2022-10-31T23:59:00",
+      "link": "https://www.gradescope.com/courses/419131/assignments/2386747/submissions/145359663",
+      "submitted": true
+    }
+  ]
+};
