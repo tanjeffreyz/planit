a6d471c1bc378ac050e8e33fc5afdb006010c63b
planit <planit@tanjeffreyz.github.io>
2022-11-05 02:34:00 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -60,8 +60,8 @@ assignments = {
       "title": "Homework 5",
       "course": "CS 161",
       "dueDate": "2022-11-04T23:59:00",
-      "link": "https://www.gradescope.com/courses/419634/assignments/2384298/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/419634/assignments/2384298/submissions/146620901",
+      "submitted": true
     },
     {
       "title": "Project 2 Design Doc Checkpoint",
