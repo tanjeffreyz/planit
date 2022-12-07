f4ed964421eac4bc25bae6c85e64172bcc845ad0
planit <planit@tanjeffreyz.github.io>
2022-12-07 10:38:12 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -81,7 +81,7 @@ assignments = {
       "title": "Project 2 Autograder",
       "course": "CS 161",
       "dueDate": "2022-12-01T23:59:00",
-      "link": "https://www.gradescope.com/courses/419634/assignments/2431060/submissions/152961523",
+      "link": "https://www.gradescope.com/courses/419634/assignments/2431060/submissions/152961593",
       "submitted": true
     },
     {
