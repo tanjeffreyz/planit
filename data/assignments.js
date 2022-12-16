3faa2e30197badcd198def52139e711b0d5b0582
planit <planit@tanjeffreyz.github.io>
2022-12-16 23:35:29 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -160,6 +160,13 @@ assignments = {
       "dueDate": "2022-12-17T16:00:00",
       "link": "https://www.gradescope.com/courses/419634/assignments/2508181/submissions/new",
       "submitted": false
+    },
+    {
+      "title": "Online Final Exam",
+      "course": "CS 161",
+      "dueDate": "2022-12-16T18:10:00",
+      "link": "https://www.gradescope.com/courses/419634/assignments/2514365/submissions/new",
+      "submitted": false
     }
   ],
   "Data 100": [
