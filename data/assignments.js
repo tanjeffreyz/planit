5b8e456866892e46776fbdf2f026800bcea20420
planit <planit@tanjeffreyz.github.io>
2023-01-17 22:35:34 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,12 @@
 assignments = {
-  "CS 162": [],
+  "CS 162": [
+    {
+      "title": "Homework 0 Written",
+      "course": "CS 162",
+      "dueDate": "2023-01-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/487063/assignments/2579753/submissions/new",
+      "submitted": false
+    }
+  ],
   "CS 184/284A": []
 };
