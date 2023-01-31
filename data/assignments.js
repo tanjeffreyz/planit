89dda4c601188a620d7510a9c9ec04153ce91558
planit <planit@tanjeffreyz.github.io>
2023-01-31 08:37:58 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -22,7 +22,15 @@ assignments = {
       "submitted": false
     }
   ],
-  "CS 184/284A": [],
+  "CS 184/284A": [
+    {
+      "title": "HW 1 Checkpoint",
+      "course": "CS 184/284A",
+      "dueDate": "2023-02-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2596089/submissions/new",
+      "submitted": false
+    }
+  ],
   "CS 188": [
     {
       "title": "[Ungraded] Project 0",
