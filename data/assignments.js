623e33032745e9e4c6b9cb543152390a1a90d8fe
planit <planit@tanjeffreyz.github.io>
2023-02-01 22:35:56 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -20,6 +20,13 @@ assignments = {
       "dueDate": "2023-02-08T23:59:00",
       "link": "https://www.gradescope.com/courses/487063/assignments/2614295/submissions/new",
       "submitted": false
+    },
+    {
+      "title": "Project 1 Design Document",
+      "course": "CS 162",
+      "dueDate": "2023-02-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/487063",
+      "submitted": false
     }
   ],
   "CS 184/284A": [
@@ -85,6 +92,13 @@ assignments = {
       "title": "[Ungraded] Project 2 Cheatsheet",
       "course": "CS 188",
       "dueDate": "2023-05-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/483556",
+      "submitted": false
+    },
+    {
+      "title": "Project 2",
+      "course": "CS 188",
+      "dueDate": "2023-02-14T23:59:00",
       "link": "https://www.gradescope.com/courses/483556",
       "submitted": false
     }
