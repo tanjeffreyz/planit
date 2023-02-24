a0638bef58d78347cc3558aed648eb54f9fbd11c
planit <planit@tanjeffreyz.github.io>
2023-02-24 16:45:54 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -66,6 +66,13 @@ assignments = {
       "submitted": false
     },
     {
+      "title": "Participation Tracker",
+      "course": "CS 184/284A",
+      "dueDate": "2023-02-24T08:00:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2665249/submissions/164035672",
+      "submitted": true
+    },
+    {
       "title": "Project 3-2 Checkpoint",
       "course": "CS 184/284A",
       "dueDate": "2023-03-21T23:59:00",
