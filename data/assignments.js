b4df76e0310c404ccaca83b5e4dd3f38bba16190
planit <planit@tanjeffreyz.github.io>
2023-03-01 08:40:21 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -101,6 +101,13 @@ assignments = {
       "submitted": false
     },
     {
+      "title": "Project 3-1: Pathtracer",
+      "course": "CS 184/284A",
+      "dueDate": "2023-03-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806",
+      "submitted": false
+    },
+    {
       "title": "Project 4: ClothSim",
       "course": "CS 184/284A",
       "dueDate": "2023-04-12T23:59:00",
