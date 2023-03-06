002f12828083a1aa853afed2314e7bacf1320cf4
planit <planit@tanjeffreyz.github.io>
2023-03-06 07:35:12 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -134,6 +134,13 @@ assignments = {
       "dueDate": "2023-04-12T23:59:00",
       "link": "https://www.gradescope.com/courses/483806",
       "submitted": false
+    },
+    {
+      "title": "Practice Exam 1",
+      "course": "CS 184/284A",
+      "dueDate": "2023-03-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2721966/submissions/new",
+      "submitted": false
     }
   ]
 };
