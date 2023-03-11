4106bdaa87d05dfb3ab614876899ffd7a3dae7d3
planit <planit@tanjeffreyz.github.io>
2023-03-11 08:36:58 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -46,7 +46,7 @@ assignments = {
       "title": "Project 2 Design Document",
       "course": "CS 162",
       "dueDate": "2023-03-10T23:59:00",
-      "link": "https://www.gradescope.com/courses/487063/assignments/2713302/submissions/167970380",
+      "link": "https://www.gradescope.com/courses/487063/assignments/2713302/submissions/167990588",
       "submitted": true
     },
     {
