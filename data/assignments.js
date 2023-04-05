3d019ac6d31e31d6ec76011c0d6cedc673a94a2c
planit <planit@tanjeffreyz.github.io>
2023-04-05 02:35:17 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -118,8 +118,8 @@ assignments = {
       "title": "Project 4 Checkpoint",
       "course": "CS 184/284A",
       "dueDate": "2023-04-04T23:59:00",
-      "link": "https://www.gradescope.com/courses/483806/assignments/2691591/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/483806/assignments/2691591/submissions/172705068",
+      "submitted": true
     },
     {
       "title": "Project 3-1: Pathtracer",
