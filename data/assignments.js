f517258ddc3b471680a761213868eaeb9268e711
planit <planit@tanjeffreyz.github.io>
2023-04-16 09:34:39 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -146,7 +146,7 @@ assignments = {
       "title": "Project 4: ClothSim",
       "course": "CS 184/284A",
       "dueDate": "2023-04-13T23:59:00",
-      "link": "https://www.gradescope.com/courses/483806/assignments/2691651/submissions/175021197",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2691651/submissions/175030470",
       "submitted": true
     },
     {
