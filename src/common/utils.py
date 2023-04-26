393b4f5911b0e1dac3ead853bb37c58ad471ec60
Gavin Zyonse <gzyonse@gmail.com>
2023-04-26 12:37:48 -0400

    Generate iCalendar file

--- src/common/utils.py
+++ src/common/utils.py
@@ -28,3 +28,24 @@ def save_data(var_name, obj):
     target = os.path.join(DATA_DIR, var_name + '.js')
     with open(target, 'w') as file:
         file.write(f'{var_name} = {json.dumps(obj, indent=2)};\n')
+    save_ical(obj)
+
+def save_ical(var_name, obj):
+    target = os.path.join(DATA_DIR, var_name + '.ics')
+    with open(target, 'w') as file:
+        # print icalendar header info
+        file.write('BEGIN:VCALENDAR\n')
+        file.write('VERSION:2.0\n')
+        file.write('PRODID:-//Gradescope//NONSGML Assignments//EN\n')
+
+        # loop through assignments in each class
+        for course in list.values():
+            for assignment in course:
+                # create event entry
+                file.write('BEGIN:VEVENT\n')
+                file.write('SUMMARY:' + assignment['title'] + '\n')
+                file.write('DTSTART:' + assignment['dueDate'].replace('-','').replace(':','') + '\n')
+                file.write('LOCATION:' + assignment['course'] + '\n')
+                file.write('URL:' + assignment['link'] + '\n')
+                file.write('END:VEVENT\n')
+        file.write('END:VCALENDAR')
