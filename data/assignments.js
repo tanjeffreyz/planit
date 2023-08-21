7fd818ab2be4a563092ae606a3c3a62a301774fc
planit <planit@tanjeffreyz.github.io>
2023-08-21 21:34:48 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,5 +1,13 @@
 assignments = {
-  "COMPSCI 164-LEC-001": [],
+  "COMPSCI 164-LEC-001": [
+    {
+      "title": "hw0",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-08-31T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216",
+      "submitted": false
+    }
+  ],
   "CS 180/280A": [],
   "CS 189/289A": [],
   "EECS 127-227AT-LEC-001": []
