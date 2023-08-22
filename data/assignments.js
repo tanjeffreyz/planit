746470bef828b02b51d989272a76b34ab5588886
planit <planit@tanjeffreyz.github.io>
2023-08-22 19:34:50 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -6,6 +6,13 @@ assignments = {
       "dueDate": "2023-08-31T21:00:00",
       "link": "https://www.gradescope.com/courses/572216",
       "submitted": false
+    },
+    {
+      "title": "Drill 1",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-08-31T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216/assignments/3092782/submissions/new",
+      "submitted": false
     }
   ],
   "CS 180/280A": [],
