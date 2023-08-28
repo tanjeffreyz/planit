2cc6fb09ab8564d5a17468b6f7d21557979e249b
planit <planit@tanjeffreyz.github.io>
2023-08-28 19:34:44 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -23,6 +23,13 @@ assignments = {
       "dueDate": "2023-09-08T23:59:00",
       "link": "https://www.gradescope.com/courses/575262",
       "submitted": false
+    },
+    {
+      "title": "HW 1 Written",
+      "course": "CS 189/289A",
+      "dueDate": "2023-09-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/575262",
+      "submitted": false
     }
   ],
   "EECS 127-227AT-LEC-001": [
