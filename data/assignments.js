0f46d2700751b654f91a490561cc6f822cf8e19e
planit <planit@tanjeffreyz.github.io>
2023-09-04 23:34:52 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -54,5 +54,13 @@ assignments = {
       "submitted": false
     }
   ],
-  "CS198-008": []
+  "CS198-008": [
+    {
+      "title": "Lab 01",
+      "course": "CS198-008",
+      "dueDate": "2023-09-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/572351/assignments/3255167/submissions/new",
+      "submitted": false
+    }
+  ]
 };
