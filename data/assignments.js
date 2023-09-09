c7991a857986137c4d6996bc8762d0ae6224d6fe
planit <planit@tanjeffreyz.github.io>
2023-09-09 06:44:22 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,1 +1,80 @@
-assignments = {};
+assignments = {
+  "COMPSCI 164-LEC-001": [
+    {
+      "title": "hw0",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-08-31T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216/assignments/3079267/submissions/188466880",
+      "submitted": true
+    },
+    {
+      "title": "Drill 1",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-08-31T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216/assignments/3092782/submissions/188472050",
+      "submitted": true
+    },
+    {
+      "title": "hw1",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-09-14T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216",
+      "submitted": false
+    },
+    {
+      "title": "Drill 2",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-09-07T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216/assignments/3208606/submissions/190301789",
+      "submitted": true
+    },
+    {
+      "title": "Drill 3",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-09-14T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216/assignments/3283256/submissions/new",
+      "submitted": false
+    }
+  ],
+  "CS 180/280A": [
+    {
+      "title": "Project 1 - Colorizing the Prokudin-Gorskii Photo Collection",
+      "course": "CS 180/280A",
+      "dueDate": "2023-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/581661/assignments/3186346/submissions/189914303",
+      "submitted": true
+    }
+  ],
+  "CS 189/289A": [
+    {
+      "title": "HW 1 Code",
+      "course": "CS 189/289A",
+      "dueDate": "2023-09-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/575262/assignments/3184862/submissions/191544416",
+      "submitted": true
+    },
+    {
+      "title": "HW 1 Written",
+      "course": "CS 189/289A",
+      "dueDate": "2023-09-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/575262/assignments/3192847/submissions/191597948",
+      "submitted": true
+    }
+  ],
+  "CS198-008": [
+    {
+      "title": "Lab 01",
+      "course": "CS198-008",
+      "dueDate": "2023-09-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/572351/assignments/3255167/submissions/190953989",
+      "submitted": true
+    },
+    {
+      "title": "Vitamin 1",
+      "course": "CS198-008",
+      "dueDate": "2023-09-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/572351/assignments/3264956/submissions/190653419",
+      "submitted": true
+    }
+  ]
+};
