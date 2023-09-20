75c3a5b267b571991145ca2846fc9e0d4e417850
planit <planit@tanjeffreyz.github.io>
2023-09-20 04:34:43 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -62,7 +62,7 @@ assignments = {
       "title": "Project 2 - Fun with Filters and Frequencies",
       "course": "CS 180/280A",
       "dueDate": "2023-09-18T23:59:00",
-      "link": "https://www.gradescope.com/courses/581661/assignments/3186347/submissions/194421252",
+      "link": "https://www.gradescope.com/courses/581661/assignments/3186347/submissions/194786810",
       "submitted": true
     }
   ],
