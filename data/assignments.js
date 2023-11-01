e3fe4a6f676e9ee4707195c1e84b248f9519b35f
planit <planit@tanjeffreyz.github.io>
2023-11-01 08:35:00 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -74,7 +74,7 @@ assignments = {
       "title": "hw6",
       "course": "COMPSCI 164-LEC-001",
       "dueDate": "2023-11-02T21:00:00",
-      "link": "https://www.gradescope.com/courses/572216/assignments/3424585/submissions/208268354",
+      "link": "https://www.gradescope.com/courses/572216/assignments/3424585/submissions/208295442",
       "submitted": true
     }
   ],
