836221ff926dc76f6652b6e0c658f26c1351f489
planit <planit@tanjeffreyz.github.io>
2023-11-04 23:34:55 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -325,8 +325,8 @@ assignments = {
       "title": "Vitamin 9",
       "course": "CS198-008",
       "dueDate": "2023-11-04T23:59:00",
-      "link": "https://www.gradescope.com/courses/572351/assignments/3610558/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/572351/assignments/3610558/submissions/209545814",
+      "submitted": true
     }
   ]
 };
