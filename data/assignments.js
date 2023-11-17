bad8f8eb6db859c81d140c83ce0404c1cdd2a83c
planit <planit@tanjeffreyz.github.io>
2023-11-17 03:35:21 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -139,7 +139,7 @@ assignments = {
       "title": "Project 5",
       "course": "CS 180/280A",
       "dueDate": "2023-11-15T23:59:00",
-      "link": "https://www.gradescope.com/courses/581661/assignments/3626531/submissions/213414729",
+      "link": "https://www.gradescope.com/courses/581661/assignments/3626531/submissions/213542038",
       "submitted": true
     }
   ],
