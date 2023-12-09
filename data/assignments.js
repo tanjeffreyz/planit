791b9cae5520c1dcb001a64990ce753c5c046824
planit <planit@tanjeffreyz.github.io>
2023-12-09 02:37:41 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -153,7 +153,7 @@ assignments = {
       "title": "[Pre-Canned] Final Project",
       "course": "CS 180/280A",
       "dueDate": "2023-12-09T23:59:00",
-      "link": "https://www.gradescope.com/courses/581661/assignments/3779474/submissions/218862322",
+      "link": "https://www.gradescope.com/courses/581661/assignments/3779474/submissions/218868149",
       "submitted": true
     }
   ],
