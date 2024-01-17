8de408fcfdf5920cdcca7fe2ac5fd5465a93c0fd
planit <planit@tanjeffreyz.github.io>
2024-01-17 04:36:34 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,11 @@
 assignments = {
-  "EECS 127/227AT-LEC-001": []
+  "EECS 127/227AT-LEC-001": [
+    {
+      "title": "Homework 01 PDF",
+      "course": "EECS 127/227AT-LEC-001",
+      "dueDate": "2024-01-19T23:00:00",
+      "link": "https://www.gradescope.com/courses/699270",
+      "submitted": false
+    }
+  ]
 };
