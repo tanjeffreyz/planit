14c159e980ae3236f8c94e29e2ab45c80d6cee3f
planit <planit@tanjeffreyz.github.io>
2024-01-28 08:34:37 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -11,7 +11,7 @@ assignments = {
       "title": "Homework 01 Self Grade",
       "course": "EECS 127/227AT-LEC-001",
       "dueDate": "2024-01-26T23:00:00",
-      "link": "https://www.gradescope.com/courses/699270/assignments/3973130/submissions/new",
+      "link": "https://www.gradescope.com/courses/699270",
       "submitted": false
     },
     {
