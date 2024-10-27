da89b9447dde38252a9f4d95a55078f5dd5fe320
planit <planit@tanjeffreyz.github.io>
2024-10-27 23:36:49 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -130,8 +130,8 @@ assignments = {
       "title": "10/28 Reading Summary & Questions",
       "course": "CS 194/294-196",
       "dueDate": "2024-10-27T23:59:00",
-      "link": "https://www.gradescope.com/courses/852247/assignments/5197544/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/852247/assignments/5197544/submissions/282614521",
+      "submitted": true
     },
     {
       "title": "Quiz 7",
