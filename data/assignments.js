7a9d78a7f51f64062f563a0b4b7024974ed72be6
planit <planit@tanjeffreyz.github.io>
2024-11-01 02:11:34 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -146,6 +146,13 @@ assignments = {
       "dueDate": "2024-11-03T23:59:00",
       "link": "https://www.gradescope.com/courses/852247/assignments/5233313/submissions/new",
       "submitted": false
+    },
+    {
+      "title": "Quiz 8",
+      "course": "CS 194/294-196",
+      "dueDate": "2024-11-04T15:10:00",
+      "link": "https://www.gradescope.com/courses/852247/assignments/5253610/submissions/new",
+      "submitted": false
     }
   ],
   "CS188": [
