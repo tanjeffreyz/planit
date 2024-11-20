409bfa93e5fe42f50894034184b21193c0ff9fd2
planit <planit@tanjeffreyz.github.io>
2024-11-20 05:40:22 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -412,8 +412,8 @@ assignments = {
       "title": "Homework 9 [Self-Assessment]",
       "course": "CS188",
       "dueDate": "2024-11-19T23:59:00",
-      "link": "https://www.gradescope.com/courses/825036",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/825036/assignments/5300506/submissions/290945460",
+      "submitted": true
     },
     {
       "title": "Homework 10 [Written Component]",
