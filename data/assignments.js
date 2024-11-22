f46ac3e7240faa9ef228c284b3e95fcb156566eb
planit <planit@tanjeffreyz.github.io>
2024-11-22 17:38:59 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -428,13 +428,6 @@ assignments = {
       "dueDate": "2024-12-03T23:59:00",
       "link": "https://www.gradescope.com/courses/825036",
       "submitted": false
-    },
-    {
-      "title": "Project 5",
-      "course": "CS188",
-      "dueDate": "2024-12-05T23:59:00",
-      "link": "https://www.gradescope.com/courses/825036",
-      "submitted": false
     }
   ]
 };
